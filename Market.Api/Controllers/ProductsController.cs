19204cae4da9b67186e6b63a25afbc0b50acb6d9
nouri <nouri@DESKTOP-42E3HIA>
2024-09-02 00:17:25 +0200

    Add productController

---
+++ Market.Api/Controllers/ProductsController.cs
@@ -0,0 +1,42 @@
+﻿using Market.Api.Dtos;
+using Maroc.Core.Interfaces.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Market.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var products = await _productService.GetAllAsync();
+                var productDtos = products.Select(p => new ProductsResponseDto
+                {
+                    Id = p.Id,
+                    Name = p.Name,
+                    Description = p.Description,
+                    Price = p.Price,
+                    ImageUrl = p.ImageUrl
+                }).ToList();
+                return Ok(productDtos);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
