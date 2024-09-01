da5a45bd30c7c8fe3f6424e43d99136a59678513
nouri <nouri@DESKTOP-42E3HIA>
2024-09-02 00:17:02 +0200

    Add scoped

--- Market.Api/Program.cs
+++ Market.Api/Program.cs
@@ -1,4 +1,8 @@
+using Maroc.Core.Interfaces;
+using Maroc.Core.Interfaces.Services;
+using Maroc.Core.Services;
 using Maroc.Infastructure.Data;
+using Maroc.Infastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +12,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultDatabase")));
 
