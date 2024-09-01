c0a065afe893fb3b2758cf544298d8e2ad3492e0
nouri <nouri@DESKTOP-42E3HIA>
2024-09-02 00:17:38 +0200

    Add productResponseDto

---
+++ Market.Api/Dtos/ProductsResponseDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Market.Api.Dtos
+{
+    public class ProductsResponseDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
