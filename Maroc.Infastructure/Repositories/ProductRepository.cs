5024397ca78a6f7699aa7b463ae4664b9cad6125
nouri <nouri@DESKTOP-42E3HIA>
2024-09-02 00:16:13 +0200

    Add repo and interface product

---
+++ Maroc.Infastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using Maroc.Core.Entities;
+using Maroc.Core.Interfaces;
+using Maroc.Infastructure.Data;
+
+namespace Maroc.Infastructure.Repositories
+{
+    public class ProductRepository : CrudRepository<Product>, IProductRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
