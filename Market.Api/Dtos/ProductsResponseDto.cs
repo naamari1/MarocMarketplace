0ea7a6231ac03fe19604b7b51a8dff9ba8872177
nouri <nouri@DESKTOP-42E3HIA>
2024-09-02 11:46:32 +0200

    add prop

--- Market.Api/Dtos/ProductsResponseDto.cs
+++ Market.Api/Dtos/ProductsResponseDto.cs
@@ -8,5 +8,6 @@
         public decimal Price { get; set; }
         public string ImageUrl { get; set; }
         public Guid CategoryId { get; set; }
+        public string CategoryName { get; set; }
     }
 }
