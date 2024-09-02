4356b9fb6443bd7fc1eca00547eff4c16d27ed15
nouri <nouri@DESKTOP-42E3HIA>
2024-09-02 11:09:34 +0200

    Add property

--- Market.Api/Dtos/ProductsResponseDto.cs
+++ Market.Api/Dtos/ProductsResponseDto.cs
@@ -7,5 +7,6 @@
         public string Description { get; set; }
         public decimal Price { get; set; }
         public string ImageUrl { get; set; }
+        public Guid CategoryId { get; set; }
     }
 }
