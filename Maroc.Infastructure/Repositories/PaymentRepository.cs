d48c1d6b92f7309c9f15d8167568441772b383a4
nouri <nouri@DESKTOP-42E3HIA>
2024-09-03 21:42:10 +0200

    Add repo and interface

---
+++ Maroc.Infastructure/Repositories/PaymentRepository.cs
@@ -0,0 +1,16 @@
+﻿using Maroc.Core.Entities;
+using Maroc.Core.Interfaces;
+using Maroc.Infastructure.Data;
+
+namespace Maroc.Infastructure.Repositories
+{
+    public class PaymentRepository : CrudRepository<Payment>, IPaymentRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PaymentRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
