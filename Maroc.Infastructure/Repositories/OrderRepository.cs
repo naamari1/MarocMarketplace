54f09f5237b6a5cf548babedace4ad1615ba6e17
nouri <nouri@DESKTOP-42E3HIA>
2024-09-03 20:06:16 +0200

    Add repo and interface

---
+++ Maroc.Infastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,15 @@
+﻿using Maroc.Core.Entities;
+using Maroc.Core.Interfaces;
+using Maroc.Infastructure.Data;
+
+namespace Maroc.Infastructure.Repositories
+{
+    public class OrderRepository : CrudRepository<Order>, IOrderRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public OrderRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
