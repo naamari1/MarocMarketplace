c132b61f1e48b1bf4ca477ee50746e2efb09c672
nouri <nouri@DESKTOP-42E3HIA>
2024-09-03 20:28:03 +0200

    Add OrdersController

---
+++ Market.Api/Controllers/OrdersController.cs
@@ -0,0 +1,69 @@
+﻿using Market.Api.Dtos;
+using Maroc.Core.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Market.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly IOrderService _orderService;
+
+        public OrdersController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                var orders = _orderService.GetAll();
+                var ordersDto = orders.Select(o => new OrdersResponseDto
+                {
+                    Id = o.Id,
+                    UserId = o.UserId,
+                    OrderDate = o.OrderDate,
+                    Status = o.Status,
+                    TotalItems = o.TotalItems
+                });
+                return Ok(ordersDto);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(Guid id)
+        {
+            try
+            {
+                var order = await _orderService.GetByIdAsync(id);
+
+                if (order == null)
+                {
+                    return NotFound();
+                }
+                var orderDto = new OrdersResponseDto
+                {
+                    Id = order.Id,
+                    UserId = order.UserId,
+                    OrderDate = order.OrderDate,
+                    Status = order.Status,
+                    TotalItems = order.TotalItems
+                };
+                return Ok(orderDto);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+
+    }
+}
