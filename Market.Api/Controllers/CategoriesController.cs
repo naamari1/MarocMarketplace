026a09bfd1bb6833f4b2eaa2915e650aa9adbcd3
AnisAamari <anis.aamari@student.howest.be>
2024-09-03 23:57:04 +0200

    Add controller and Dto

---
+++ Market.Api/Controllers/CategoriesController.cs
@@ -0,0 +1,18 @@
+﻿using Maroc.Core.Interfaces.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Market.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+    }
+}
---
+++ Market.Api/Dtos/CategoriesResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Market.Api.Dtos
+{
+    public class CategoriesResponseDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
