49da921c48d987ca337948c9feadec505eea5c53
nouri <nouri@DESKTOP-42E3HIA>
2024-09-03 21:42:58 +0200

    Add controller + dto

---
+++ Market.Api/Dtos/PaymentResponseDto.cs
@@ -0,0 +1,13 @@
+﻿using Maroc.Core.Enums;
+
+namespace Market.Api.Dtos
+{
+    public class PaymentResponseDto
+    {
+        public Guid Id { get; set; }
+        public Guid OrderId { get; set; }
+        public decimal Amount { get; set; }
+        public PaymentMethod PaymentMethod { get; set; }
+        public DateTime PaymentDate { get; set; }
+    }
+}
