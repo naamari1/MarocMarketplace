a52302fcc013067e4e82b1942d745c22794733d5
nouri <nouri@DESKTOP-42E3HIA>
2024-09-03 20:28:15 +0200

    Add dto

---
+++ Market.Api/Dtos/OrdersResponseDto.cs
@@ -0,0 +1,13 @@
+﻿using Maroc.Core.Enums;
+
+namespace Market.Api.Dtos
+{
+    public class OrdersResponseDto
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public OrderStatus Status { get; set; }
+        public int TotalItems { get; set; }
+    }
+}
