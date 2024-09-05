9bbeb4562bdace71067c613634bf5ee52051a02e
nouri <nouri@DESKTOP-42E3HIA>
2024-09-05 12:07:46 +0200

    Modify confic react

--- marocmarketplace.react/src/App.jsx
+++ marocmarketplace.react/src/App.jsx
@@ -1,49 +1,31 @@
-import { useEffect, useState } from 'react';
+import React from 'react';
 import './App.css';
+import { Routes, Route, Link } from 'react-router-dom';
+import Products from './Products';  // Import the Products component
 
 function App() {
-    const [forecasts, setForecasts] = useState();
-
-    useEffect(() => {
-        populateWeatherData();
-    }, []);
-
-    const contents = forecasts === undefined
-        ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
-        : <table className="table table-striped" aria-labelledby="tabelLabel">
-            <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>Temp. (C)</th>
-                    <th>Temp. (F)</th>
-                    <th>Summary</th>
-                </tr>
-            </thead>
-            <tbody>
-                {forecasts.map(forecast =>
-                    <tr key={forecast.date}>
-                        <td>{forecast.date}</td>
-                        <td>{forecast.temperatureC}</td>
-                        <td>{forecast.temperatureF}</td>
-                        <td>{forecast.summary}</td>
-                    </tr>
-                )}
-            </tbody>
-        </table>;
-
     return (
-        <div>
-            <h1 id="tabelLabel">Weather forecast</h1>
-            <p>This component demonstrates fetching data from the server.</p>
-            {contents}
+        <div className="App">
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>  // Link to the homepage
+                    </li>
+                    <li>
+                        <Link to="/products">Products</Link>  // Link to the Products page
+                    </li>
+                </ul>
+            </nav>
+            <Routes>
+                <Route path="/" element={<Home />} />  // Route for the homepage
+                <Route path="/products" element={<Products />} />  // Route for the Products page
+            </Routes>
         </div>
     );
-    
-    async function populateWeatherData() {
-        const response = await fetch('weatherforecast');
-        const data = await response.json();
-        setForecasts(data);
-    }
+}
+
+function Home() {
+    return <h1>Welcome to the Homepage</h1>;  // Content for the homepage
 }
 
 export default App;
